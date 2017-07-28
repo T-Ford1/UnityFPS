66a660477368a8e04cab6e315c35c55866c328d3
ford.terrell <ford.terrell3@gmail.com>
2017-07-28 11:52:05 -0500

    created temp world and basic input

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float playerSpeed = 5f;
+    [SerializeField]
+    private float playerLook = 5f;
+
+    private PlayerMotor motor;
+
+	// Use this for initialization
+	void Start () {
+        motor = GetComponent<PlayerMotor>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //calculate movement as a 3D vector
+
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _movHorizontal = transform.right * _xMov;
+        Vector3 _movVertical = transform.forward * _zMov;
+
+        //final movement vector
+        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * playerSpeed;
+
+        motor.MovePlayer(_velocity);
+
+        //calculate rotation as a 3D vector
+
+        float _yRot = Input.GetAxisRaw("Mouse X");//move mouse on x, rotate around the y axis
+
+        Vector3 _playerRotation = new Vector3(0f, _yRot, 0f) * playerLook;
+
+        //Apply rotation
+        motor.RotatePlayer(_playerRotation);
+
+
+        float _xRot = -Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _cameraRotation = new Vector3(_xRot, 0f, 0f) * playerLook;
+
+        //Apply rotation
+        motor.RotateCamera(_cameraRotation);
+    }
+}
---
+++ Assets/PlayerMotor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+
+    private Vector3 playerVelocity = Vector3.zero;
+    private Vector3 playerRotation = Vector3.zero;
+    private Vector3 cameraRotation = Vector3.zero;
+
+    private Rigidbody rb;
+
+    [SerializeField]
+    private Camera cam;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+
+    internal void MovePlayer(Vector3 _playerVelocity)
+    {
+        playerVelocity = _playerVelocity;
+    }
+
+    internal void RotatePlayer(Vector3 _playerRotation)
+    {
+        playerRotation = _playerRotation;
+    }
+
+    internal void RotateCamera(Vector3 _cameraRotation)
+    {
+        cameraRotation = _cameraRotation;
+    }
+
+    // FixedUpdate is called once per physics update
+    void FixedUpdate () {
+        PerformMovement();
+        PerformRotation();
+	}
+
+    
+    void PerformMovement()
+    {
+        if(playerVelocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + playerVelocity * Time.fixedDeltaTime);
+        }
+    }
+
+    void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(playerRotation));
+        if(cam != null)
+        {
+            cam.transform.Rotate(cameraRotation);
+        }
+    }
+}
