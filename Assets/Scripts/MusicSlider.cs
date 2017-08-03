1e8cf39c54e14acf78524eac8123f4ccfbee14ce
Christopher <chris1376@hotmail.com>
2017-08-03 16:57:52 -0500

    added an intro screen gui

---
+++ Assets/Scripts/MusicSlider.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+[RequireComponent(typeof(AudioSource))]
+public class MusicSlider : MonoBehaviour {
+
+    private Slider s;
+    private AudioSource a;
+
+	void Start () {
+        s = GetComponent<Slider>();
+        a = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        a.volume = s.value;
+        if(s.value == 0f)
+        {
+            a.Pause();
+        } else
+        {
+            a.UnPause();
+        }
+	}
+}
