d9a1358ecf0a87da5dcd1c56130b455d8cbf4418
ford.terrell <ford.terrell3@gmail.com>
2017-08-04 17:21:19 -0500

    made the play button play

--- Assets/Scripts/MusicSlider.cs
+++ Assets/Scripts/MusicSlider.cs
@@ -3,25 +3,38 @@ using UnityEngine.UI;
 
 [RequireComponent(typeof(Slider))]
 [RequireComponent(typeof(AudioSource))]
-public class MusicSlider : MonoBehaviour {
+public class MusicSlider : MonoBehaviour
+{
+
+    [SerializeField]
+    private Toggle t;
 
     private Slider s;
     private AudioSource a;
 
-	void Start () {
+    void Start()
+    {
         s = GetComponent<Slider>();
         a = GetComponent<AudioSource>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        a.volume = s.value;
-        if(s.value == 0f)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(t != null && !t.isOn)
         {
             a.Pause();
-        } else
+            return;
+        }
+
+        a.volume = s.value;
+        if (s.value == 0f)
+        {
+            a.Pause();
+        }
+        else
         {
             a.UnPause();
         }
-	}
+    }
 }
---
+++ Assets/Scripts/PlayScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Button))]
+public class PlayScript : MonoBehaviour {
+
+    [SerializeField]
+    private string loadingScreenName;
+    [SerializeField]
+    private string gameSceneName;
+
+    private Button b;
+
+	// Use this for initialization
+	void Start () {
+        b = GetComponent<Button>();
+        b.onClick.AddListener(() => OnButtonClick());
+	}
+
+    void OnButtonClick()
+    {
+        Scene _load = SceneManager.CreateScene(loadingScreenName);
+        SceneManager.LoadScene(loadingScreenName);
+        SceneManager.SetActiveScene(_load);
+
+        Scene _game = SceneManager.CreateScene(gameSceneName);
+        SceneManager.LoadScene(gameSceneName);
+        SceneManager.SetActiveScene(_game);
+
+        //AsynchronousLoad();
+    }
+
+    IEnumerator AsynchronousLoad(string scene)
+    {
+        yield return null;
+
+        AsyncOperation ao = SceneManager.LoadSceneAsync(scene);
+        ao.allowSceneActivation = false;
+
+        while (!ao.isDone)
+        {
+            // [0, 0.9] > [0, 1]
+            float progress = Mathf.Clamp01(ao.progress / 0.9f);
+            Debug.Log("Loading progress: " + (progress * 100) + "%");
+
+            // Loading completed
+            if (ao.progress == 0.9f)
+            {
+                Debug.Log("Press a key to start");
+                if (Input.anyKey)
+                    ao.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+}
